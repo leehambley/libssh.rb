c597e87e00f48080495e68ff9b08bce5b8706eb0
Lee Hambley <lee.hambley@gmail.com>
2011-04-29 22:22:50 +0200

    Couple of stray, uncommitted files - not useful yet

--- ext/libssh.rb/extconf.rb
+++ ext/libssh.rb/extconf.rb
@@ -2,8 +2,10 @@ require 'mkmf'
 
 unless find_header('libssh.h')
   abort("libssh.h was not found, please install the package")
+end
   
 unless find_library('ssh2', 'libssh_session_init_ex')
   abort("libssh was not found, please install the package")
+end
 
 create_makefile 'libssh.rb'
