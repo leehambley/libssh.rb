1c34d330171a79d34374fed55ccded2bf290a328
Lee Hambley <lee.hambley@gmail.com>
2011-05-09 23:46:52 +0200

    Ensure that the mkmf looks in some sane places (/usr/local/{lib,include}) for libssh, and look for the correct symbol

--- ext/libssh.rb/extconf.rb
+++ ext/libssh.rb/extconf.rb
@@ -1,11 +1,13 @@
 require 'mkmf'
 
-unless find_header('libssh.h')
+dir_config 'libssh', '/opt/local/include', '/opt/local/lib'
+
+unless find_header('libssh/libssh.h')
   abort("libssh.h was not found, please install the package")
 end
-  
-unless find_library('ssh2', 'libssh_session_init_ex')
-  abort("libssh was not found, please install the package")
+
+unless find_library('ssh', 'ssh_new')
+  abort("`ssh_new()` was not found in libssh, please check that the package is correctly installed")
 end
 
 create_makefile 'libssh.rb'
