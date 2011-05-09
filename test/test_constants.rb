9e96e92c1fdf753b237a8005c8f030d7c48758b8
Lee Hambley <lee.hambley@gmail.com>
2011-05-10 00:42:10 +0200

    Tiny tweak, preferring to implicitly infer the correct namespace

--- test/test_constants.rb
+++ test/test_constants.rb
@@ -2,6 +2,6 @@ require_relative 'helper'
 
 class TestConstants < MiniTest::Unit::TestCase
   def test_libssh_constant_defined
-    assert defined?(::LibSSH)
+    assert defined?(LibSSH)
   end
 end
